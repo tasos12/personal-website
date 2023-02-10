a561c96bb7cd9cc014d4c3d2fcccdc57333c298c
Gkagkas Anastasios <tasosggps@gmail.com>
2023-02-11 00:54:56 +0200

    Fix project grid and add technology section to each project page.

--- pages/mse/index.js
+++ pages/mse/index.js
@@ -1,26 +1,39 @@
 import Image from "next/image";
 import Head from "next/head";
-import Container from "@mui/material/Container";
-import "react-slideshow-image/dist/styles.css";
-import { Slide } from "react-slideshow-image";
-import Chip from "@mui/material/Chip";
 import Navbar from "/components/navbar";
 import Footer from "/components/footer";
+import "react-slideshow-image/dist/styles.css";
+import { Slide } from "react-slideshow-image";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import ArrowCircleRightOutlined from "@mui/icons-material/ArrowCircleRightOutlined";
+import List from "@mui/material/List";
+import ListSubheader from "@mui/material/ListSubheader";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import Html5Original from "devicons-react/lib/icons/Html5Original";
+import Css3Original from "devicons-react/lib/icons/Css3Original";
+import JavascriptOriginal from "devicons-react/lib/icons/JavascriptOriginal";
+import PhpOriginal from "devicons-react/lib/icons/PhpOriginal";
+import MongodbOriginal from "devicons-react/lib/icons/MongodbOriginal";
+import DockerOriginal from "devicons-react/lib/icons/DockerOriginal";
 
+import LeafletJS from "/public/images/shared/leafletjs.ico";
 import overviewImage from "/public/images/mse/overview.png";
 
 export default function Home() {
     const pages = ["overview", "about", "technologies"];
     const sectionStyle = { margin: "20px 0" };
-    const cardChipStyle = {
-        my: 1,
-        mx: 0.5,
-        p: 0.5,
+    const technologyContainerStyle = {
+        background: "rgb(30, 30, 80)",
+        paddingTop: "5px",
         color: "white",
-        background: "black",
+        display: "flex",
+        flexDirection: "column",
     };
 
     return (
@@ -88,7 +101,7 @@ export default function Home() {
                     <Typography className="header1-alt-text" component={"h2"}>
                         About the project
                     </Typography>
-                    <Typography className="header2-text" component={"section"}>
+                    <Typography className="normal-text" component={"section"}>
                         MSE is a web application developed for CALLISTO a
                         HORIZON 2020 program that allows the end user: 1. To
                         search and retrieve UAV images from a vast range of
@@ -100,28 +113,100 @@ export default function Home() {
                     <Typography className="header1-alt-text" component={"h2"}>
                         Technologies
                     </Typography>
-                    <div>
-                        <Chip
-                            className="normal-text"
-                            sx={cardChipStyle}
-                            label="MongoDB"
-                        />
-                        <Chip
-                            className="normal-text"
-                            sx={cardChipStyle}
-                            label="PHP"
-                        />
-                        <Chip
-                            className="normal-text"
-                            sx={cardChipStyle}
-                            label="Javascript"
-                        />
-                        <Chip
-                            className="normal-text"
-                            sx={cardChipStyle}
-                            label="HTML/CSS"
-                        />
-                    </div>
+                    <Container className="responsive-grid" sx={{ gap: "20px" }}>
+                        <Paper elevation={2} sx={technologyContainerStyle}>
+                            <List disablePadding={true}>
+                                <ListSubheader
+                                    sx={{
+                                        background: "rgb(30, 30, 80)",
+                                        color: "white",
+                                        fontWeight: "700",
+                                    }}
+                                >
+                                    Front-end
+                                </ListSubheader>
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <Html5Original size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>HTML</ListItemText>
+                                </ListItem>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <Css3Original size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>CSS</ListItemText>
+                                </ListItem>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <JavascriptOriginal size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>Javascript</ListItemText>
+                                </ListItem>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <Image
+                                            src={LeafletJS}
+                                            alt="leafletjs"
+                                            width="24"
+                                        />
+                                    </ListItemIcon>
+                                    <ListItemText>LeafletJs</ListItemText>
+                                </ListItem>
+                            </List>
+                        </Paper>
+
+                        <Paper sx={technologyContainerStyle}>
+                            <List disablePadding={true}>
+                                <ListSubheader
+                                    sx={{
+                                        background: "rgb(30, 30, 80)",
+                                        color: "white",
+                                        fontWeight: "700",
+                                    }}
+                                >
+                                    Back-end
+                                </ListSubheader>
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <PhpOriginal size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>PHP</ListItemText>
+                                </ListItem>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <MongodbOriginal size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>MongoDB</ListItemText>
+                                </ListItem>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <DockerOriginal size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>Docker</ListItemText>
+                                </ListItem>
+                            </List>
+                        </Paper>
+                    </Container>
                 </section>
                 <Footer />
             </Container>
--- pages/smas/index.js
+++ pages/smas/index.js
@@ -1,28 +1,46 @@
 import Image from "next/image";
 import Head from "next/head";
-import Container from "@mui/material/Container";
+import Navbar from "/components/navbar";
+import Footer from "/components/footer";
 import "react-slideshow-image/dist/styles.css";
 import { Slide } from "react-slideshow-image";
-import Chip from "@mui/material/Chip";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import ArrowCircleRightOutlined from "@mui/icons-material/ArrowCircleRightOutlined";
-import Navbar from "/components/navbar";
-import Footer from "/components/footer";
+import List from "@mui/material/List";
+import ListSubheader from "@mui/material/ListSubheader";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import ReactOriginal from "devicons-react/lib/icons/ReactOriginal";
+import LaravelOriginal from "devicons-react/lib/icons/LaravelOriginal";
+import JavaOriginal from "devicons-react/lib/icons/JavaOriginal";
+import MongodbOriginal from "devicons-react/lib/icons/MongodbOriginal";
+import DockerOriginal from "devicons-react/lib/icons/DockerOriginal";
 
+import LeafletJS from "/public/images/shared/leafletjs.ico";
+import ChartJS from "/public/images/shared/chartjs.ico";
 import overviewImage from "/public/images/smas/overview.png";
 import overview2Image from "/public/images/smas/overview2.png";
 import overview3Image from "/public/images/smas/overview3.png";
 
 export default function Home() {
-    const pages = ["overview", "about", "contribution", "technologies"];
+    const pages = ["overview", "about", "technologies"];
     const sectionStyle = { margin: "20px 0" };
-    const cardChipStyle = {
-        my: 1,
-        mx: 0.5,
-        p: 0.5,
+    const technologyContainerStyle = {
+        background: "rgb(30, 30, 80)",
+        paddingTop: "5px",
         color: "white",
-        background: "black",
+        display: "flex",
+        flexDirection: "column",
+    };
+    const listSubheaderStyle = {
+        background: "rgb(30, 30, 80)",
+        color: "white",
+        fontWeight: "700",
     };
     const imageStyle = { maxWidth: "100%", height: "auto", objectFit: "cover" };
 
@@ -99,18 +117,13 @@ export default function Home() {
                     <Typography className="header1-alt-text" component={"h2"}>
                         About the project
                     </Typography>
-                    <Typography className="header2-text" component={"section"}>
+                    <Typography className="normal-text" component={"p"}>
                         SMAS is a social media data monitoring and retrieval app
                         that allows the end user to retrieve filtered data and
                         analytics from various social media platforms and
                         visulize them in a dashboard environment.
                     </Typography>
-                </section>
-                <section id="contribution" style={sectionStyle}>
-                    <Typography className="header1-alt-text" component={"h2"}>
-                        My contribution
-                    </Typography>
-                    <Typography className="header2-text" component={"section"}>
+                    <Typography className="normal-text" component={"p"}>
                         I was responsible for the design and development of the
                         frontend and backend of the project using ReactJS and
                         Laravel respectively. I also developed the database
@@ -123,33 +136,84 @@ export default function Home() {
                     <Typography className="header1-alt-text" component={"h2"}>
                         Technologies
                     </Typography>
-                    <div>
-                        <Chip
-                            className="normal-text"
-                            sx={cardChipStyle}
-                            label="ReactJs"
-                        />
-                        <Chip
-                            className="normal-text"
-                            sx={cardChipStyle}
-                            label="Laravel"
-                        />
-                        <Chip
-                            className="normal-text"
-                            sx={cardChipStyle}
-                            label="MongoDB"
-                        />
-                        <Chip
-                            className="normal-text"
-                            sx={cardChipStyle}
-                            label="Java"
-                        />
-                        <Chip
-                            className="normal-text"
-                            sx={cardChipStyle}
-                            label="Docker"
-                        />
-                    </div>
+                    <Container className="responsive-grid" sx={{ gap: "20px" }}>
+                        <Paper elevation={2} sx={technologyContainerStyle}>
+                            <List disablePadding={true}>
+                                <ListSubheader sx={listSubheaderStyle}>
+                                    Front-end
+                                </ListSubheader>
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <ReactOriginal size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>ReactJS</ListItemText>
+                                </ListItem>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <Image src={LeafletJS} alt="leafletjs" width="24"/>
+                                    </ListItemIcon>
+                                    <ListItemText>LeafletJS</ListItemText>
+                                </ListItem>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <Image src={ChartJS} alt="chartjs" width="24"/>
+                                    </ListItemIcon>
+                                    <ListItemText>Chart.js</ListItemText>
+                                </ListItem>
+                            </List>
+                        </Paper>
+
+                        <Paper sx={technologyContainerStyle}>
+                            <List disablePadding={true}>
+                                <ListSubheader sx={listSubheaderStyle}>
+                                    Back-end
+                                </ListSubheader>
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <LaravelOriginal size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>Laravel</ListItemText>
+                                </ListItem>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <JavaOriginal size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>Java</ListItemText>
+                                </ListItem>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <DockerOriginal size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>Docker</ListItemText>
+                                </ListItem>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemIcon
+                                        sx={{ justifyContent: "center" }}
+                                    >
+                                        <MongodbOriginal size="24" />
+                                    </ListItemIcon>
+                                    <ListItemText>MongoDB</ListItemText>
+                                </ListItem>
+                            </List>
+                        </Paper>
+                    </Container>
                 </section>
                 <Footer />
             </Container>
